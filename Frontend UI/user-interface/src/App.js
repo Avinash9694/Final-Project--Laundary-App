667bec9cf15c6d6f16017bfa36073b6f33b968c1
ChetamoniRamudu <ramudu1307@gmail.com>
2022-03-24 10:36:45 +0530

    "secondcommit"

--- Frontend UI/user-interface/src/App.js
+++ Frontend UI/user-interface/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import Navbar from "./components/Navbar.js";
+import Footer from "./components/footer.js";
+import SignIn from "./components/Sign In.js";
+import Register from "./components/Register.js";
+import "./App.css";
+import {BrowserRouter,Route,Switch} from "react-router-dom";
+const app = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="homepage">
+        <BrowserRouter>
+        <Navbar />
+        <Switch>
+            <Route path="/signin" exact component={SignIn}/>
+            <Route path="/signup" exact component={Register}/>
+        </Switch>
+        <Footer/>
+        </BrowserRouter>
+    </section>
   );
-}
-
-export default App;
+};
+export default app;
---
+++ Frontend UI/user-interface/src/components/footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+const Footer=()=>{
+    return(
+        <div className="footer">
+            <div className="footerbox1">
+                <h4 className="message">Now Refer&amp;Earn {'\u20A8'}500 for every referral*</h4>
+                <p className="condition">*Terms and conditions will be applied</p>
+
+            </div>
+            <div className="footerbox2">
+                    <h6 className="about">ABOUT US</h6>
+                    <p className="about1">Doorstep Wash &amp; Dryclean Service</p>
+                    <h6 className="information1">Home</h6>
+                    <p className="information2">Sign In</p>
+                    <p className="information3">Register</p>
+                    <h6 className="informationpricing1">Pricing</h6>
+                    <h6 className="informationcareer">Career</h6>
+                    <p className="informationblogs">Blogs</p>
+                    <p className="informationcreate">Create</p>
+                    <h6 className="informationcontact">Contact</h6>
+                    <h4 className="informationmedia">SOCIAL MEDIA</h4>
+                    <span className="facebook"></span>
+                    <span className="instagram"></span>
+                    <span className="linkedin"></span>
+                    <p className="footerout">2021 &copy;Laundry</p>
+            </div>
+        </div>
+    )
+}
+export default Footer
